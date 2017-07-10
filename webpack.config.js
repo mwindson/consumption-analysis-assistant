b44e658a8af9e295824fea2d30d123dc1ee7f929
Jiabing Yu <mwindson@163.com>
2017-07-10 10:56:56 +0800

    添加index，修改依赖和配置

--- webpack.config.js
+++ webpack.config.js
@@ -26,23 +26,37 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        loaders: ['style-loader', 'css-loader'],
       },
       {
         test: /\.styl$/,
-        use: ['style-loader', 'css.style-loader', 'stylus-loader'],
+        loaders: ['style-loader', 'css-loader', 'stylus-loader'],
       },
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['transform-react-jsx', 'transform-class-properties'],
+            }
+          }
+        ]
       }
     ],
   },
 
   resolve: {
-    modules: ['node_modules', path.resolve(__dirname, 'app')],
-    extensions: ['.js', '.jsx'],
+    // 解析模块请求的选项
+    // （不适用于对 loader 解析）
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'app'),
+    ],
+    // 用于查找模块的目录
+    extensions: ['.js', '.json', '.jsx', '.css'],
+    // 使用的扩展名
   },
 
   plugins: [
